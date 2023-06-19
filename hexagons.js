579bc9da2712b8bc2f422ef3cf52c92c2716c3e0
Christopher Brown <cjb4@williams.edu>
2023-06-19 18:15:37 -0400

    The hexagonal board with correct spacing has been done with more css and much less javascript; easier to read

--- hexagons.js
+++ hexagons.js
@@ -1,10 +1,14 @@
-var side_length = 10;
+var side_length = window.innerWidth / 33;
 var root3 = 1.73205;
-var height = 5 * 2;
+var height = 2 * side_length;
 var width = root3 * side_length;
 var border_bottom_top = side_length - (side_length / 2.0);
 var border_left_right = (root3 / 2.0) * side_length;
 var grid_container = document.getElementById("grid-container");
+if (grid_container != null) {
+    grid_container.style.gridTemplateRows = "repeat(21, minmax(" + height + "px, " + height + "px))";
+    grid_container.style.gridTemplateColumns = "repeat(21, minmax(" + width + "px, " + width + "px))";
+}
 var row = 1;
 while (row < 12) {
     var col = 1;
@@ -24,8 +28,13 @@ while (row < 12) {
         grid_container === null || grid_container === void 0 ? void 0 : grid_container.appendChild(div_elem);
         var sheet = window.document.styleSheets[0];
         var num = col + row;
+        var right_offset = (width / 2.0) * (row - 1);
+        var top_offset = -7 * (row - 1);
         var style = '#' + div_elem.id +
             ' {\n'
+            + 'position: relative;'
+            + 'right: ' + right_offset + 'px;'
+            + 'top: ' + top_offset + 'px;'
             + 'grid-column: ' + num + ';\n'
             + 'grid-row: ' + row + ';\n}';
         sheet.insertRule(style, sheet.cssRules.length);
@@ -42,7 +51,7 @@ while (row < 12) {
             + 'grid-column: ' + col + ';\n'
             + 'grid-row: ' + row + ';\n'
             + 'width: ' + width + 'px;\n'
-            + 'height: ' + height + 'px;\n'
+            + 'height: ' + side_length + 'px;\n'
             + 'background: #6C8;'
             + '\n}';
         sheet.insertRule(style_middle, sheet.cssRules.length);
