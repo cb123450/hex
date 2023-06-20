91d1f6d49f44413a6ae40879ed19d0267bca8910
Christopher Brown <cjb4@williams.edu>
2023-06-20 09:38:35 -0400

    got blue squares; found parsing error

--- hexagons.js
+++ hexagons.js
@@ -10,75 +10,105 @@ if (grid_container != null) {
     grid_container.style.gridTemplateColumns = "repeat(21, minmax(" + width + "px, " + width + "px))";
 }
 var row = 1;
-while (row < 12) {
+while (row < 14) {
     var col = 1;
-    while (col < 12) {
+    while (col < 14) {
         //Create new html element
-        var div_elem = document.createElement("div");
-        div_elem.id = 'r' + row + 'c' + col;
-        var div_elem_top = document.createElement("div");
-        div_elem_top.id = 'r' + row + 'c' + col + "_top";
-        var div_elem_middle = document.createElement("div");
-        div_elem_middle.id = 'r' + row + 'c' + col + "_middle";
-        var div_elem_bottom = document.createElement("div");
-        div_elem_bottom.id = 'r' + row + 'c' + col + "_bottom";
-        div_elem.appendChild(div_elem_top);
-        div_elem.appendChild(div_elem_middle);
-        div_elem.appendChild(div_elem_bottom);
-        grid_container === null || grid_container === void 0 ? void 0 : grid_container.appendChild(div_elem);
+        var hex_container = document.createElement("div");
+        hex_container.id = 'r' + row + 'c' + col;
+        grid_container === null || grid_container === void 0 ? void 0 : grid_container.appendChild(hex_container);
         var sheet = window.document.styleSheets[0];
         var num = col + row;
         var right_offset = (width / 2.0) * (row - 1);
-        var top_offset = -7 * (row - 1);
-        var style = '#' + div_elem.id +
+        var top_offset = -9 * (row - 1);
+        var style = '#' + hex_container.id +
             ' {\n'
             + 'position: relative;'
+            //+ 'width: ' + width + ';'
+            //+ 'height: ' + height + ';'
             + 'right: ' + right_offset + 'px;'
             + 'top: ' + top_offset + 'px;'
+            //+ 'box-sizing: content-box;'
+            //+ 'padding-right: 100%;'
             + 'grid-column: ' + num + ';\n'
             + 'grid-row: ' + row + ';\n}';
         sheet.insertRule(style, sheet.cssRules.length);
-        var style_top = '#' + div_elem_top.id +
-            ' {\n'
-            + 'width: 0;'
-            + 'border-bottom: ' + border_bottom_top + 'px solid #6C8;'
-            + 'border-left: ' + border_left_right + 'px solid transparent;'
-            + 'border-right: ' + border_left_right + 'px solid transparent;'
-            + '\n}';
-        sheet.insertRule(style_top, sheet.cssRules.length);
-        var style_middle = '#' + div_elem_middle.id +
-            ' {\n'
-            + 'grid-column: ' + col + ';\n'
-            + 'grid-row: ' + row + ';\n'
-            + 'width: ' + width + 'px;\n'
-            + 'height: ' + side_length + 'px;\n'
-            + 'background: #6C8;'
-            + '\n}';
-        sheet.insertRule(style_middle, sheet.cssRules.length);
-        //Add buttons
-        var b = document.createElement("button");
-        div_elem_middle.appendChild(b);
-        b.addEventListener("click", function () {
-            alert("did something");
-        });
-        b.id = div_elem_middle.id + '_button';
-        var button_style = '#' + b.id +
-            ' {\n'
-            + 'height: ' + side_length + 'px;\n'
-            + 'width: ' + width + 'px;\n'
-            + 'background-color: rgb(102, 204, 136);\n'
-            + 'border: 0px;\n'
-            + ''
-            + '\n}';
-        sheet.insertRule(button_style, sheet.cssRules.length);
-        var style_bottom = '#' + div_elem_bottom.id +
-            ' {\n'
-            + 'width: 0;'
-            + 'border-top: ' + border_bottom_top + 'px solid #6C8;'
-            + 'border-left: ' + border_left_right + 'px solid transparent;'
-            + 'border-right: ' + border_left_right + 'px solid transparent;'
-            + '\n}';
-        sheet.insertRule(style_bottom, sheet.cssRules.length);
+        if (row == 1) {
+            var upper_border = document.createElement("div");
+            upper_border.id = 'r' + row + 'c' + col + "_upper_border";
+            hex_container.appendChild(upper_border);
+            console.log(upper_border.id);
+            var style_upper_border = '#' + upper_border.id
+                + ' {\n'
+                + 'width: 0;'
+                + 'border-top: ' + 10 + 'px solid blue;'
+                + 'border-bottom: ' + 10 + 'px solid blue;'
+                + 'border-left: ' + 10 + 'px solid blue;'
+                + 'border-right: ' + 10 + 'px solid blue;'
+                + '\n}';
+            sheet.insertRule(style_upper_border, sheet.cssRules.length);
+        }
+        else if (row == 13) {
+        }
+        else if (col == 1) {
+        }
+        else if (col == 13) {
+        }
+        else {
+            //UPPER
+            var upper = document.createElement("div");
+            upper.id = 'r' + row + 'c' + col + "_upper";
+            hex_container.appendChild(upper);
+            var style_upper = '#' + upper.id +
+                ' {\n'
+                + 'width: 0;'
+                + 'border-bottom: ' + border_bottom_top + 'px solid #6C8;'
+                + 'border-left: ' + border_left_right + 'px solid transparent;'
+                + 'border-right: ' + border_left_right + 'px solid transparent;'
+                + '\n}';
+            sheet.insertRule(style_upper, sheet.cssRules.length);
+            //MIDDLE
+            var middle = document.createElement("div");
+            middle.id = 'r' + row + 'c' + col + "_middle";
+            hex_container.appendChild(middle);
+            var style_middle = '#' + middle.id +
+                ' {\n'
+                + 'grid-column: ' + col + ';\n'
+                + 'grid-row: ' + row + ';\n'
+                + 'width: ' + width + 'px;\n'
+                + 'height: ' + side_length + 'px;\n'
+                + 'background: #6C8;'
+                + '\n}';
+            sheet.insertRule(style_middle, sheet.cssRules.length);
+            //Add buttons
+            var b = document.createElement("button");
+            middle.appendChild(b);
+            b.addEventListener("click", function () {
+                alert("did something");
+            });
+            b.id = middle.id + '_button';
+            var button_style = '#' + b.id +
+                ' {\n'
+                + 'height: ' + side_length + 'px;\n'
+                + 'width: ' + width + 'px;\n'
+                + 'background-color: rgb(102, 204, 136);\n'
+                + 'border: 0px;\n'
+                + ''
+                + '\n}';
+            sheet.insertRule(button_style, sheet.cssRules.length);
+            //LOWER
+            var lower = document.createElement("div");
+            lower.id = 'r' + row + 'c' + col + "_lower";
+            hex_container.appendChild(lower);
+            var style_lower = '#' + lower.id +
+                ' {\n'
+                + 'width: 0;'
+                + 'border-top: ' + border_bottom_top + 'px solid #6C8;'
+                + 'border-left: ' + border_left_right + 'px solid transparent;'
+                + 'border-right: ' + border_left_right + 'px solid transparent;'
+                + '\n}';
+            sheet.insertRule(style_lower, sheet.cssRules.length);
+        }
         col += 1;
     }
     row += 1;
--- hexagons.ts
+++ hexagons.ts
@@ -12,90 +12,125 @@ if (grid_container != null){
 }
 
 var row : number = 1;
-while (row < 12){
+while (row < 14){
     var col : number = 1;
-    while (col < 12){
+    while (col < 14){
         //Create new html element
-        var div_elem : HTMLDivElement = document.createElement("div");
-        div_elem.id = 'r' + row + 'c' + col;
+        var hex_container : HTMLDivElement = document.createElement("div");
+        hex_container.id = 'r' + row + 'c' + col;
 
-        var div_elem_top : HTMLDivElement = document.createElement("div");
-        div_elem_top.id = 'r' + row + 'c' + col + "_top";
-
-        var div_elem_middle : HTMLDivElement = document.createElement("div");
-        div_elem_middle.id = 'r' + row + 'c' + col + "_middle";
-
-        var div_elem_bottom : HTMLDivElement = document.createElement("div");
-        div_elem_bottom.id = 'r' + row + 'c' + col + "_bottom";
-
-        div_elem.appendChild(div_elem_top);
-        div_elem.appendChild(div_elem_middle);
-        div_elem.appendChild(div_elem_bottom);
-
-        grid_container?.appendChild(div_elem);
+        grid_container?.appendChild(hex_container);
 
         var sheet = window.document.styleSheets[0];
 
         var num = col + row;
         var right_offset = (width/2.0)*(row-1);
-        var top_offset = -7 * (row-1);
+        var top_offset = -9 * (row-1);
 
-        var style = '#' + div_elem.id + 
+        var style = '#' + hex_container.id + 
         ' {\n' 
         + 'position: relative;'
+        //+ 'width: ' + width + ';'
+        //+ 'height: ' + height + ';'
         + 'right: ' + right_offset + 'px;'
         + 'top: ' + top_offset + 'px;'
+        //+ 'box-sizing: content-box;'
+        //+ 'padding-right: 100%;'
         + 'grid-column: ' + num + ';\n' 
         + 'grid-row: ' + row + ';\n}';
         sheet.insertRule(style, sheet.cssRules.length);
 
-        var style_top = '#' + div_elem_top.id + 
-        ' {\n' 
-        + 'width: 0;'
-        + 'border-bottom: ' + border_bottom_top+ 'px solid #6C8;'
-        + 'border-left: ' + border_left_right+ 'px solid transparent;'
-        + 'border-right: ' + border_left_right+ 'px solid transparent;'
-        + '\n}';
-        sheet.insertRule(style_top, sheet.cssRules.length);
+        
+        if (row == 1){
+            var upper_border : HTMLDivElement = document.createElement("div");            
+            upper_border.id = 'r' + row + 'c' + col + "_upper_border";
+            hex_container.appendChild(upper_border);
+            console.log(upper_border.id);
 
-        var style_middle = '#' + div_elem_middle.id + 
-        ' {\n' 
-        + 'grid-column: ' + col + ';\n' 
-        + 'grid-row: ' + row + ';\n'
-        + 'width: ' + width + 'px;\n' 
-        + 'height: ' + side_length + 'px;\n' 
-        + 'background: #6C8;'
-        +'\n}';
-        sheet.insertRule(style_middle, sheet.cssRules.length);
+            var style_upper_border = '#' + upper_border.id
+            + ' {\n' 
+            + 'width: 0;'
+            + 'border-top: ' +  10 + 'px solid blue;'
+            + 'border-bottom: ' + 10 + 'px solid blue;'
+            + 'border-left: ' + 10 + 'px solid blue;'
+            + 'border-right: ' + 10 + 'px solid blue;'
+             + '\n}';
+            sheet.insertRule(style_upper_border, sheet.cssRules.length);
+        }
+        else if (row == 13){
 
-        //Add buttons
-        var b = document.createElement("button");        
-        div_elem_middle.appendChild(b);
-        b.addEventListener("click", function(){
-            alert("did something");
-        });
+        }
+        else if (col == 1){
 
-        b.id = div_elem_middle.id + '_button';
+        }
+        else if (col == 13){
 
-        var button_style = '#' + b.id + 
-        ' {\n' 
-        + 'height: ' + side_length + 'px;\n' 
-        + 'width: ' + width + 'px;\n'
-        + 'background-color: rgb(102, 204, 136);\n'
-        + 'border: 0px;\n'
-        + ''
-        +'\n}';
+        }
+        else{
+            //UPPER
+            var upper : HTMLDivElement = document.createElement("div");
+            upper.id = 'r' + row + 'c' + col + "_upper";
+            hex_container.appendChild(upper);
 
-        sheet.insertRule(button_style, sheet.cssRules.length);
+            var style_upper = '#' + upper.id + 
+            ' {\n' 
+            + 'width: 0;'
+            + 'border-bottom: ' + border_bottom_top+ 'px solid #6C8;'
+            + 'border-left: ' + border_left_right+ 'px solid transparent;'
+            + 'border-right: ' + border_left_right+ 'px solid transparent;'
+            + '\n}';
+            sheet.insertRule(style_upper, sheet.cssRules.length);
 
-        var style_bottom = '#' + div_elem_bottom.id + 
-        ' {\n' 
-        + 'width: 0;'
-        + 'border-top: ' + border_bottom_top+ 'px solid #6C8;'
-        + 'border-left: ' + border_left_right+ 'px solid transparent;'
-        + 'border-right: ' + border_left_right+ 'px solid transparent;'
-        + '\n}';
-        sheet.insertRule(style_bottom, sheet.cssRules.length);
+
+            //MIDDLE
+            var middle : HTMLDivElement = document.createElement("div");
+            middle.id = 'r' + row + 'c' + col + "_middle";
+            hex_container.appendChild(middle);
+
+            var style_middle = '#' + middle.id + 
+            ' {\n' 
+            + 'grid-column: ' + col + ';\n' 
+            + 'grid-row: ' + row + ';\n'
+            + 'width: ' + width + 'px;\n' 
+            + 'height: ' + side_length + 'px;\n' 
+            + 'background: #6C8;'
+            +'\n}';
+            sheet.insertRule(style_middle, sheet.cssRules.length);
+
+            //Add buttons
+            var b = document.createElement("button");        
+            middle.appendChild(b);
+            b.addEventListener("click", function(){
+                alert("did something");
+            });
+
+            b.id = middle.id + '_button';
+
+            var button_style = '#' + b.id + 
+            ' {\n' 
+            + 'height: ' + side_length + 'px;\n' 
+            + 'width: ' + width + 'px;\n'
+            + 'background-color: rgb(102, 204, 136);\n'
+            + 'border: 0px;\n'
+            + ''
+            +'\n}';
+
+            sheet.insertRule(button_style, sheet.cssRules.length);
+
+            //LOWER
+            var lower : HTMLDivElement = document.createElement("div");
+            lower.id = 'r' + row + 'c' + col + "_lower";    
+            hex_container.appendChild(lower);
+
+            var style_lower = '#' + lower.id + 
+            ' {\n' 
+            + 'width: 0;'
+            + 'border-top: ' + border_bottom_top+ 'px solid #6C8;'
+            + 'border-left: ' + border_left_right+ 'px solid transparent;'
+            + 'border-right: ' + border_left_right+ 'px solid transparent;'
+            + '\n}';
+            sheet.insertRule(style_lower, sheet.cssRules.length);
+        }
         
         col += 1;
     }
