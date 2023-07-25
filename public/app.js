a52e8ac978d252a420853f16065aed9c750fcb6f
Christopher Brown <cjb4@williams.edu>
2023-07-24 20:53:02 -0400

    reorganization: original-hex-chrome and new-backend-hex

--- public/app.js
+++ public/app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static( './views' + '/public'));
+
+app.set("view engine", "ejs")
+
 app.get('/', (req, res) => {
     console.log("here")
-    res.download('server.js')
+    res.render("index")
 })
 
 app.listen(3000)
