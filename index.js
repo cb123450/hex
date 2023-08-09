cfb806cbcf0866466edf3a7edc92a13660158b29
Christopher Brown <cjb4@williams.edu>
2023-08-09 10:10:29 -0400

    changed CORS policy

--- index.js
+++ index.js
@@ -53,6 +53,11 @@ io.on("connection", (socket) =>{
   })
 })
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(express.json())
 
